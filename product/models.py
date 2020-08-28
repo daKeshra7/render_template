751a7e78703cf058aba5b324cd858ebb24dbe35d
daKeshra7 <keshradamora@gmail.com>
2020-08-28 10:12:21 +0100

    Initial

---
+++ product/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Products(models.Model):
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField(default="Write your experience")
